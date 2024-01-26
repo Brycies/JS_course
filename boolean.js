c01e216e8057ab2ff920a7af2a7a7c92c1e9ee89
Brycies <brysonodhiambo29@gmail.com>
2024-01-26 12:14:06 +0300

    creating script files

---
+++ boolean.js
@@ -0,0 +1,26 @@
+const age = 15;
+
+if (age >= 16) {
+  console.log("You can drive a car");
+} else if (age >= 15) {
+  console.log("You are almost there");
+} else {
+  console.log("Oh my I am so sorry, until next time");
+}
+// const year0ld = 13;
+// console.log(year0ld);
+
+console.log(true && true);
+console.log(true && false);
+
+console.log(0.2 >= 0 && 0.2 < 1 / 3);
+
+console.log(true || false);
+console.log(!true);
+console.log(!false);
+
+let cartQuantity = 2;
+
+if ((cartQuantity = 3)) {
+  console.log("Cart has products");
+}
---
+++ cartQuantity.js
@@ -0,0 +1,3 @@
+let cartQuantity = 3;
+cartQuantity = cartQuantity + 1;
+console.log(cartQuantity);
