46485a0b3975994b91da0870a8207efb3e6cb1b1
Brycies <brysonodhiambo29@gmail.com>
2024-01-29 11:35:53 +0300

    call stack

---
+++ callStack.js
@@ -0,0 +1,16 @@
+// function greet(who) {
+//   console.log("Hello " + who);
+// }
+// greet("Bryson");
+// console.log("Bye Bryson");
+
+function chicken() {
+  return egg();
+}
+function egg() {
+  return chicken();
+}
+console.log(chicken() + " came first.");
+// storing this stack the computer required space in hte memory, when the stack
+// grows too big the computer will fail with a message like 'out of stack space' or
+//' too much recurssion
