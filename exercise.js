383f2dfe475b1d1b4bd36354b003a088c733f5f8
Brycies <brysonodhiambo29@gmail.com>
2024-02-01 17:57:09 +0300

    exercise course

---
+++ exercise.js
@@ -0,0 +1,24 @@
+let yourName = "William Odhiambo";
+const age = 19;
+if ((age = 24)) {
+  console.log("You can drink");
+} else if (age >= 18) {
+  console.log("Don't drink and drive");
+} else {
+  console.log("You are underage");
+}
+console.log(yourName);
+
+function name() {
+  myName = "Bryson";
+  myAge = 16;
+}
+if (age >= 21) {
+  console.log("You are allowed to drink");
+} else if (age <= 18) {
+  console.log("Illegal to persons under the age of 18");
+} else {
+  console.log("Dont drink and drive");
+}
+myAge = 17;
+console.log(myName);
